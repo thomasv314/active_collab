1bd0e295ec0a418e781a3201973ab9fe79bcc7b1
Thomas Vendetta <thomas.vendetta@ajj.com>
2012-12-21 13:47:33 -0500

    Still refactoring the test suite.

--- spec/lib/active_collab_spec.rb
+++ spec/lib/active_collab_spec.rb
@@ -1,11 +1,24 @@
 require "spec_helper"
 
-
 describe 'ActiveCollab' do 
 
+  before :each do
+    @client = ActiveCollab::Client.new(API_URL, API_KEY)
+  end
+
+
   describe '#initialize' do
-    client = ActiveCollab::Client.new(API_URL, API_KEY)
-    client.class.should eq(ActiveCollab::Client)
+    it "should return an Active Collab client" do  
+      @client.class.should eq(ActiveCollab::Client)
+    end 
+  end
+
+  describe '#projects' do
+
+    it "should return an array of projects" do
+      @client.projects.class.should eq(Array)
+    end
+
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,8 +8,8 @@
 require 'active_collab'
 config = YAML::load(File.join(File.dirname(File.expand_path(__FILE__)), 'client.yml'))
 
-API_URL = config['client']['api_url']
-API_KEY = config['client']['api_key']
+@API_URL = config['client']['api_url']
+@API_KEY = config['client']['api_key']
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
