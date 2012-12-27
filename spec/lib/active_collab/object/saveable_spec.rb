263fa285aa11c831fc8cc770c108ef68e8bae830
Thomas Vendetta <thomas.vendetta@ajj.com>
2012-12-27 15:03:56 -0500

    Cleaner spec

--- spec/lib/active_collab/object/saveable_spec.rb
+++ spec/lib/active_collab/object/saveable_spec.rb
@@ -4,58 +4,48 @@ describe ActiveCollab::Object::Saveable do
 
   class TestClass < ActiveCollab::Object::Record 
     attr_accessor :pid, :id
+    has_save_routes({ 
+      :create => "/projects/:id",
+      :update => "/projects/:id/edit",
+    })
+  end
+
+  before :each do 
+    @obj = TestClass.new
   end
 
   describe "#save" do
     it "returns a boolean" do
-      t = TestClass.new
-      t.methods.include?(:save).should eq(true)
+      @obj.methods.include?(:save).should eq(true)
     end
   end
 
   describe "#saved?" do
     it "tells you whether an object has been saved" do
-      t = TestClass.new
-      t.saved?.should eq(false)
+      @obj.saved?.should eq(false)
 
-      t.id = 5
-      t.saved?.should eq(true)
+      @obj.id = 5
+      @obj.saved?.should eq(true)
     end
   end
 
   describe "#build_route_string" do
     it "takes symbolized urls and returns a url based off object attributes" do  
-      t = TestClass.new
-      t.pid = 5
-      t.id = 13
+      @obj.pid = 5
+      @obj.id = 13
       string = "/parent_object/:pid/object/:id"
-      t.build_route(string).should eq("/parent_object/5/object/13")
+      @obj.build_route("/parent_object/:pid/object/:id").should eq("/parent_object/5/object/13")
     end
   end
 
   describe ".has_save_routes" do
-    class SaveRoutesTest 
-      include ActiveCollab::Object::Saveable
-      attr_accessor :id
-      has_save_routes({ 
-        :create => "/projects/:id",
-        :edit => "/projects/:id/edit",
-        :bananas => "/projects/:id/bananas"
-      })
-    end
-
     it "should create an instance method for each key its hash has.." do
-      srt = SaveRoutesTest.new
-      srt.methods.include?(:bananas_object_path).should eq(true)
-      srt.methods.include?(:create_object_path).should eq(true)
+      @obj.methods.include?(:create_object_path).should eq(true)
     end
 
     it "the instance methods for each key should return data pertinent to that instance" do 
-      srt = SaveRoutesTest.new
-      srt.id = 5
-
-      srt.bananas_object_path.should eq("/projects/5/bananas")
-      srt.create_object_path.should eq("/projects/5")
+      @obj.id = 5
+      @obj.create_object_path.should eq("/projects/5")
     end
   end
 
