470143d2e4fa8c44e232c0e912b7389eb19ddbb8
Thomas Vendetta <thomas.vendetta@ajj.com>
2013-01-03 11:21:38 -0500

    Changed attr_accessor to has_attributes in ActiveCollab::Project

--- lib/active_collab/project.rb
+++ lib/active_collab/project.rb
@@ -9,7 +9,7 @@ class ActiveCollab::Project < ActiveCollab::Object::Record
     :update => "/projects/:id/edit"
   })
 
-  attr_accessor :id, :name, :permalink, :verbose_type, :verbose_type_lowercase, :urls, :permissions, :created_on, :created_by_id, :updated_on, :updated_by_id, :state, :is_archived, :is_trashed, :completed_on, :completed_by_id, :is_completed, :avatar, :category_id, :label_id, :is_favorite, :custom_fields, :icon, :overview, :overview_formatted, :currency_code, :based_on, :status_verbose, :progress, :budget, :leader_id, :company_id 
+  has_attributes :id, :name, :permalink, :verbose_type, :verbose_type_lowercase, :urls, :permissions, :created_on, :created_by_id, :updated_on, :updated_by_id, :state, :is_archived, :is_trashed, :completed_on, :completed_by_id, :is_completed, :avatar, :category_id, :label_id, :is_favorite, :custom_fields, :icon, :overview, :overview_formatted, :currency_code, :based_on, :status_verbose, :progress, :budget, :leader_id, :company_id 
 
   def tasks
     if !@client.nil?
